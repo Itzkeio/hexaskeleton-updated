556f19348f58b70533a5db2d947c96aff6229602
keio <keiotanri@gmail.com>
2025-12-17 08:05:44 +0700

    memperbaiki filter PIC type individual

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -21,27 +21,15 @@ class CalendarController extends Controller
         $picType = $request->query('type', 'individual'); // default individual
         
         if ($picType === 'group') {
-            // Ambil project dengan PIC group
+            // Ambil HANYA project dengan PIC group
             $projects = Projects::where('picType', 'group')
                 ->where('picId', $picId)
                 ->get();
         } else {
-            // Ambil project individual + group yang user jadi member
-            $individual = Projects::where('picType', 'individual')
-                ->where('picId', $picId);
-
-            $group = Projects::where('picType', 'group')
-                ->whereIn('id', function($q) use ($picId) {
-                    $q->select('projectId')
-                      ->from('groups')
-                      ->whereIn('id', function($q2) use ($picId) {
-                          $q2->select('group_id')
-                             ->from('group_members')
-                             ->where('user_id', $picId);
-                      });
-                });
-
-            $projects = $individual->union($group)->get();
+            // Ambil HANYA project dengan PIC individual
+            $projects = Projects::where('picType', 'individual')
+                ->where('picId', $picId)
+                ->get();
         }
 
         // Mapping ke event calendar
